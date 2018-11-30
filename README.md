9729def38e6a3eddc37000b5ad80c5802bfe31f5
Matthieu BERGER <berger@braincorporation.com>
2018-11-29 17:21:09 -0800

    Implements last features

--- README.md
+++ README.md
@@ -1,9 +1,43 @@
 # Simple TCP server written in Go.
 
-I wrote this programm to get familiar with the net package in go. My final goal is to continue to learn about go and
-code using go routines and concurrency patterns.
+I wrote this program to get familiar with the net package and go routines. 
+My main objective here is to continue to learn about Go.
+Here is a list of goals I want to achieve:
 
-## Being concurrent
+- [x] Write a simple tcp server to handle connections
+- [x] Use goroutines to handle connections 
+- [x] Encoding the request with SHA1 algorithm
+- [x] Sending response and close connection
+
+## 1 - Accepting a connection
+
+The net package provide a simple function `net.Listen`  to start to listen on a specific port.
+It return a `Listener` which implement the `Accept ` function.
+
+Here is what my first lines:
+
+```go
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		// Use fatal to exit if the listener fails to start
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		break
+	}
+}
+
+
+
+ 
+## 2 - Concurrency
 The implementation of the main() function tells our TCP server to start a new goroutine each time it
 has to serve a TCP client:
 
@@ -31,3 +65,20 @@ The net.Listen() call is used to accept network connections and thus act as a se
 is of the net.Conn type, which implements both io.Reader and io.Writer interfaces. The for loop allows our program to
 keep accepting new TCP clients using Accept() that will be handled by instances of the handle() function, which are
 executed as goroutines.
+
+## 3 - Encoding the request
+
+This point is just to avoid to return the client the request itself. So I just wrote a simple function to encode the 
+request with a sha1 algorithm.
+```go
+// SHA1 hashes using sha1 algorithm
+func SHA1(text string) string {
+	algorithm := sha1.New()
+	algorithm.Write([]byte(text))
+	return hex.EncodeToString(algorithm.Sum(nil))
+}
+```
+
+## 4 - Sending response and close connection
+
+
--- main.go
+++ main.go
@@ -2,24 +2,83 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"io"
 	"log"
 	"net"
 )
 
-func handle(conn net.Conn) {
-	log.Printf("Now serving: %s \n", conn.RemoteAddr().String())
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		ln := scanner.Text()
-		log.Println(ln)
-	}
-	defer conn.Close()
+const (
+	EXIT_COMMAND = "exit"
+)
 
+// Read message from a net.Conn
+func Read(conn net.Conn) (string, error) {
+	reader := bufio.NewReader(conn)
+	var buffer bytes.Buffer
+	for {
+		ba, isPrefix, err := reader.ReadLine()
+		if err != nil {
+			// if the error is an End Of File this is still good
+			if err == io.EOF {
+				break
+			}
+			return "", err
+		}
+		buffer.Write(ba)
+		if !isPrefix {
+			break
+		}
+	}
+	return buffer.String(), nil
+}
+
+// Write message to a net.Conn
+// Return the number of bytes returned
+func Write(conn net.Conn, encoded string) (int, error) {
+	writer := bufio.NewWriter(conn)
+	number, err := writer.WriteString(encoded)
+	if err == nil {
+		err = writer.Flush()
+	}
+	return number, err
+}
+
+// SHA1 hashes using sha1 algorithm
+func SHA1(text string) string {
+	algorithm := sha1.New()
+	algorithm.Write([]byte(text))
+	return hex.EncodeToString(algorithm.Sum(nil))
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+	log.Printf("Now listnen: %s \n", conn.RemoteAddr().String())
+	content, err := Read(conn)
+	if err != nil {
+		log.Printf("Listener: Read error: %s", err)
+	}
+	if content == EXIT_COMMAND {
+		log.Println("Listener: Exit!")
+		return
+	}
+	log.Printf("Listener: Received content: %s\n", content)
+	response := fmt.Sprintf("Encoded: %s \n", SHA1(content))
+	log.Printf("Listener: Response: %s\n", response)
+	num, err := Write(conn, response)
+	if err != nil {
+		log.Printf("Listener: Write Error: %s\n", err)
+	}
+	log.Printf("Listener: Wrote %d byte(s) to %s \n", num, conn.RemoteAddr().String())
 }
 
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
+		// Use fatal to exit if the listener fails to start
 		log.Fatal(err)
 	}
 	defer listener.Close()
